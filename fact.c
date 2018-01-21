e8ad990d97238c5aaf60a4643cb333cce21d2c33
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-01-21 11:52:42 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -2,15 +2,15 @@
  
 int main()
 {
-  int c, n, fact = 1;
+  int c, v, fact = 1;
  
   printf("Enter a number to calculate its factorial\n");
-  scanf("%d", &n);
+  scanf("%d", &v);
  
-  for (c = 1; c <= n; c++)
+  for (c = 1; c <= v; c++)
     fact = fact * c;
  
-  printf("Factorial of %d = %d\n", n, fact);
+  printf("Factorial of %d = %d\n", v, fact);
  
   return 0;
 }
