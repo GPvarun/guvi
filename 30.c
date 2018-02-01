0f7b846c8523dfb70d4a9fa46c9c4511c4f505e8
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-02-01 08:01:28 -0300

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+int a,b,c=24,d=60,e,f,i,j,g,h,k,l;
+printf("enter the time");
+scanf("%d %d",&a,&b);
+e=c-a;
+f=d-b;
+printf("%d %d",e,f);
+printf("enter the time");
+scanf("%d %d",&g,&h);
+i=c-g;
+j=d-h;
+printf("%d %d",i,j);
+k=e-i;
+l=f-j;
+printf("%d %d",k,l);
+	return 0;
+}
