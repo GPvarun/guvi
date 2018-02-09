0b4c4289535f6c702a4c1d23b5adcd9d1981309b
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-02-09 14:47:51 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[200];
+    int n,count = 0, i,a;
+ 
+   printf("enter the string\n");
+   gets(s);
+   n=strlen(s);
+    for (i = 0;i<strlen(s);i++)
+    {
+        if (s[i] == '.')
+            count++;    
+    }
+    printf("number of paragraph in given string are: %d\n", count);
+    return 0;
+}
