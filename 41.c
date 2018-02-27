e3a4498e8c874658957e127d6333fd7d4b546eed
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-02-27 15:59:06 +0530

    Update 41.c

--- 41.c
+++ 41.c
@@ -3,11 +3,13 @@
 int main(void) {
 int b,i;
 char a[10];
+printf("enter the word");
 scanf("%s",&a);
+printf("how many times");
 scanf("%d",&b);
 for(i=0;i<b;i++)
 {
-printf("%c",a);
+printf("\n%s",a);
 }
 	return 0;
 }
