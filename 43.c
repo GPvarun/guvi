6f49597c02562d88d3937ec250333ad02c0ec0a2
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-02-27 16:19:43 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+char a[10],b[10];
+printf("enter the word");
+scanf("%s",&a);
+printf("enter the word");
+scanf("%s",&b);
+printf("\n%s%s",a,b);
+	return 0;
+}
