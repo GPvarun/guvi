fbcda3c5fd3891eaec238a9841803886b2ed4770
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-03-02 15:59:07 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,b,c,d,e;
+    printf("enter the numbers");
+scanf("%d",&n);
+
+{
+b=n%10;
+c=n/10;
+d=c%10;
+e=n/100;
+
+printf("%d %d %d",e,d,b);
+
+
+   } return 0;
+}
+
+
