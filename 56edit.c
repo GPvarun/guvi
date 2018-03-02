f81c1a478d646dee21c255f50c627b32ecca1ca8
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-03-02 16:52:29 +0530

    Create 56edit.c

---
+++ 56edit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char n[30],v,a;
+    printf("enter the numbers");
+scanf("%s",&n);
+if((n=='a' && n=='z') || (n=='A' && n=='Z')||(n=='0'&& n=='9'))
+{
+printf("yes");
+}
+else 
+{
+printf("no");
+}
+    return 0;
+}
+
