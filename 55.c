21c58330f31273eca6e12a8fbccb7347af9dd641
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-03-02 16:21:37 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,v,a;
+    printf("enter the numbers");
+scanf("%d %d",&n,&a);
+
+v=n*a;
+if(v%2==0)
+printf("even");
+else
+printf("odd");
+    return 0;
+}
