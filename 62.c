b95c5732c72ea701ee6d609ad149707fee65f938
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-03-13 16:40:06 +0530

    Update 62.c

--- 62.c
+++ 62.c
@@ -1,27 +1,23 @@
-
-
-#include <stdio.h>
-
-int main()
+#include<stdio.h>
+#include<string.h>
+void main()
 {
-    int i,n,count=0;
-    char a[100];
-    printf("enter the number");
-    scanf("%s",&a[100]);
-    n=strlen(a);
+char s[100]={"10001"};
+int i,n=5,flag=0;
 for(i=0;i<n;i++)
 {
-    if((n=='0')||(n=='1'))
+if(s[i]=='0'||s[i]=='1')
 {
- count++;   
-}
-}
-if(n==count)
-{
-printf("is binary");
+flag=1;
 }
 else
 {
-printf("is not binary");
- }   return 0;
+flag=0;
+break;
+}
+}
+if(flag==1)
+printf("given string is binary");
+else
+printf("not a binary");
 }
