57faecd7d441ebcde39fc4a91749ef86c58bde66
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-03-15 16:03:52 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int v,a,r;
+printf("Enter the number");
+scanf("%d %d",&a,&r);
+v=a+r;
+if(v%2==0)
+{
+printf("even number");
+}
+else
+{
+printf("odd number");
+return 0;
+}
+}
