58dbb6b4d08d1a03228d09b0a1c19b57491f9a7f
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-03-15 15:59:53 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[100]={"hai friend"};
+int i,n=100,count=1;
+for(i=0;i<n;i++)
+{
+if(s[i]==' ')
+{
+count++;
+}
+else
+{
+}
+}
+printf("%d",count);
+
+    return 0;
+}
