739fd39eb1169e4ba0571028389874c878d7f96e
GPvarun <30828544+GPvarun@users.noreply.github.com>
2018-03-20 16:40:17 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+ 
+  printf("enter the number 1&2");
+  scanf("%d %d %d",&a,&c,&b);
+    if ((a<b) && (b<c))
+      printf("yes");
+      
+    else
+      printf("no" ); 
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
